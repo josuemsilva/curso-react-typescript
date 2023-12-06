9a3df7588645c063d506f4dd30b4926443dc7b3c
JooJC <joojmoutinho45@gmail.com>
2023-12-06 17:55:34 -0300

    feat: made login and dashboard routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import { Dashboard } from "../pages";
+import { Dashboard, Login } from "../pages";
 
 export const AppRoutes = () => {
   return (
       <Routes>
+        <Route path="/entrar" element={<Login/>}/>
         <Route path="/pagina-inicial" element={<Dashboard/>}/>
 
         <Route path='*' element={<Navigate to="/pagina-inicial"/>}/>
