20bbc87d77f77714e978145144d6b0ca41ba2ed4
JooJC <joojmoutinho45@gmail.com>
2023-12-28 16:24:22 -0300

    feat: API implementation

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,13 +1,23 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
-interface ITarefa {
-  id: number;
-  title: string;
-  isCompleted: boolean;
-}
+import {
+  ITarefa,
+  TarefasService,
+} from "../../shared/services/api/tarefas/TarefasService";
+import { ApiException } from "../../shared/services/api/ApiException";
 
 export const Dashboard = () => {
   const [lista, setLista] = useState<ITarefa[]>([]);
+
+  useEffect(() => {
+    TarefasService.getAll().then((result) => {
+      if (result instanceof ApiException) {
+        alert(result.message);
+      } else {
+        setLista(result);
+      }
+    });
+  }, []);
 
   const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> =
     useCallback((e) => {
